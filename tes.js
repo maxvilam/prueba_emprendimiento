bfd817d0800a98530db844e1a817c2486dfa54ca
MaxAvila <maxvilam@hotmail.con>
2022-02-06 02:38:08 -0300

    ok estructura y mov svg

---
+++ tes.js
@@ -0,0 +1,16 @@
+const q="ww"+2+3
+const t1="Hola0";
+const t2="Hola1";
+const t3=true;
+const alumno1="Victor"
+const alumno2="Castaña"
+const alumno3="Matias"
+
+const si=`${t1} ${t3} ${t2}`;
+const alumnos =`Los alunos de front 19 son:
+        ${alumno1},
+        ${alumno2},
+        ${alumno3}.
+`;
+
+console.log(alumnos)
